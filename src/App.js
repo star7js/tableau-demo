3c62e1098ef45f34becccefae114b93a95fa51d3
Josh <jsimnitt@gmail.com>
2024-05-14 16:59:04 -0600

    added to CSS file, removed inline CSS from App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import * as React from "react";
 import "./App.css";
-import { TableauEmbed } from "@stoddabr/react-tableau-embed-live";
+import * as React from "react";
+
+import {TableauEmbed} from "@stoddabr/react-tableau-embed-live";
 
 function App() {
     return (
         <div
             className="App"
-            style={{
-                alignItems: "center",
-                display: "flex",
-                justifyContent: "center",
-                flexDirection: "column",
-            }}
         >
             <h1>The Tableau Embedded API v3</h1>
-            <h3>Now more lightweight, and backwards compatible!</h3>
+            <h3>Now more lightweightz, and backwards compatible!</h3>
             <TableauEmbed
                 sourceUrl="https://public.tableau.com/views/WorldIndicators/GDPpercapita"
                 width="1000px"
-                height="1000px"
+                height="900px"
                 options={{
                     device: "desktop",
                 }}
@@ -26,4 +21,5 @@ function App() {
         </div>
     );
 }
+
 export default App;
